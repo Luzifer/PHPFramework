a48842c57b26c4df741f1f064738635f78afbeb5
Knut Ahlers <knut@ahlers.me>
2011-05-26 22:23:18 +0200

    Added some more friendly fist site

--- templates/index.php
+++ templates/index.php
@@ -1,1 +1,23 @@
-<p>Ich bin ja SOOOO glücklich, dass ich hier auf dieser Seite stehen darf...</p>
+<p class="title">
+  Welcome to this tiny PHP-framework
+</p>
+<p>
+  I'm glad you made it here. This means you already have a running
+  instance of this framework. Now, this is just a default site to
+  show you something other than an error message on your first visit.
+</p>
+<p>
+  Things you should try now:
+</p>
+<ol>
+  <li>Look at the configuration at <em>/config/settings.init</em></li>
+  <li>Define your own URL handler at <em>/config/urls.php</em></li>
+  <li>Write your own handler class and place them to <em>/handler/</em></li>
+  <li>Modify the existing or write your own templates in <em>/templates/</em></li>
+  <li>Place your static resources somewhere in <em>/public/</em></li>
+</ol>
+<p>
+  If you have done all of that points I'm pretty sure you got how this
+  framework is supposed to work. Now you can build nearly everything 
+  with this. You are missing something? Just implement it!
+</p>
--- templates/internal/layout.php
+++ templates/internal/layout.php
@@ -2,13 +2,24 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title></title>
+    <title>404 - Page not found</title>
     <style type="text/css" media="screen">
       @import url(http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold);
-      body { font-family:'Droid Sans'; font-size:12px; }
+      @import url(http://fonts.googleapis.com/css?family=Droid+Sans+Mono);
+      body, html { font-family:'Droid Sans'; font-size:12px; height:100%; margin:0;padding:0; }
+      .swc0 { margin: 0 auto; display: table; width:600px; height:100%; }
+      .swc1 { display: table-cell; vertical-align: middle; }
+      .frame { border:1px solid #0400ae; background-color:#e6e6f7; padding:10px; border-radius:10px; margin-bottom:30px; }
+      .title { text-align:center; font-weight:bold; font-size:14px; }
     </style>
   </head>
   <body>
-    <? echo tpl::load($_actiontpl); ?>
+    <div class="swc0">
+      <div class="swc1">
+        <div class="frame">
+          <? echo tpl::load($_actiontpl); ?>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
