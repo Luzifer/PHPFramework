04ed2855ffde2719939eca075d2e70ca82a327c5
Knut Ahlers <knut@ahlers.me>
2011-05-26 22:32:03 +0200

    Changed main page advice to / and others to 404

--- config/urls.php
+++ config/urls.php
@@ -1,5 +1,7 @@
 <?php
 
+// I suggest to use | as an regex delimiter because in URLs you
+// will use / a lot and have to escape them all otherwise.
 $urlpattern = array(
-  '|.?|' => 'MainHandler'
+  '|^/$|' => 'MainHandler'
 );
