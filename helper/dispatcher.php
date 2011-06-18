53498a431687ff04badf69303fd8515a82303a13
Knut Ahlers <knut@ahlers.me>
2011-06-18 17:52:17 +0200

    Added support for disabling template caching in settings file

--- helper/dispatcher.php
+++ helper/dispatcher.php
@@ -21,7 +21,7 @@ class Dispatcher {
     
     c::set('twig.root', realpath(dirname(__file__) . '/../templates'));
     c::set('twig.debug', $config->get('debug', 0) == 1);
-    if(!is_dir(c::get('twig.root' . '/cache'))) {
+    if(!is_dir(c::get('twig.root' . '/cache')) || !$config->get('templatecache', true)) {
       c::set('twig.cache', false);
     }
   }
