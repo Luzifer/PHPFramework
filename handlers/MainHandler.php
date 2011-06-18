761cda8933949923351ec059e4ce5a1aaa6b8e49
Knut Ahlers <knut@ahlers.me>
2011-06-18 17:51:49 +0200

    Added hint for installation to support caching

--- handlers/MainHandler.php
+++ handlers/MainHandler.php
@@ -4,6 +4,7 @@ class MainHandler extends HttpHandler {
   
   function get($params) {
     tpl::set('title', 'Welcome to your new PHP-framework instance');
+    tpl::set('hascache', is_dir(c::get('twig.root' . '/cache')));
     $this->do_layout('index');
   }
   
