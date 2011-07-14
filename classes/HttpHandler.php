7519c16ee590b5989e1d460c3ae9f51a657d8d3b
Knut Ahlers <knut@ahlers.me>
2011-07-14 15:48:30 +0200

    Added JavaDoc compatible documentation

--- classes/HttpHandler.php
+++ classes/HttpHandler.php
@@ -1,7 +1,18 @@
 <?php
 
+/**
+  Base class for any handler used in urls.php
+  
+  @author Knut Ahlers <knut@ahlers.me>
+*/
 class HttpHandler {
   
+  /**
+    Short call to do a redirect to a new URL
+    
+    @param url (string) The full URL used in the redirect
+    @param code (int) May be 301 (permanent redirect) or 302 (temporary redirect)
+  */
   protected function redirect($url, $code = 302) {
     if($code == 301) {
       header("HTTP/1.1 301 Moved Permanently");
@@ -11,12 +22,37 @@ class HttpHandler {
     header('Location: ' . $url);
   }
   
+  /**
+    Wrapper for template loading
+    
+    @param template (string) Name of the template to execute
+  */
   protected function do_layout($template) {
     tpl::load($template);
   }
   
+  /**
+    Handler for GET requests
+    
+    @param params (string[]) Selections from the url defined in urls.php are passed to this
+    @throws HttpResponderException When not implemented in child class
+  */
   public function get($params) { throw new HttpResponderException('Method GET not implemented for ' . get_class($this)); }
+  
+  /**
+    Handler for HEAD requests
+
+    @param params (string[]) Selections from the url defined in urls.php are passed to this    
+    @throws HttpResponderException When not implemented in child class
+  */
   public function head($params) { throw new HttpResponderException('Method HEAD not implemented for ' . get_class($this)); }
+  
+  /**
+    Handler for POST requests
+    
+    @param params (string[]) Selections from the url defined in urls.php are passed to this
+    @throws HttpResponderException When not implemented in child class
+  */
   public function post($params) { throw new HttpResponderException('Method POST not implemented for ' . get_class($this)); }
 }
 
--- helper/Config.php
+++ helper/Config.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+  Implements a accessor to the settings.ini configuration
+  file. The file is parsed according to the currently set
+  apache environment to support for example different database
+  configurations for live-, development- and testingserver.
+  
+  @author Knut Ahlers <knut@ahlers.me>
+*/
 class Config {
   private static $instance = null;
   private static $config = null;
@@ -10,6 +18,13 @@ class Config {
     $this->env = apache_getenv('APPLICATION_ENV');
   }
   
+  /**
+    Retrieve an instance of the configuration class with loaded
+    configuration file stored to memory to prevent multiple access
+    to the settings file.
+    
+    @returns Singleton instance of Config class
+  */
   public static function getInstance() {
     if(self::$instance == null) {
       self::$instance = new self;
@@ -17,6 +32,15 @@ class Config {
     return self::$instance;
   }
   
+  /**
+    Configuration is read from the current environment section, if not
+    existent from the general config and if not present in the whole 
+    config from the passed default value.
+    
+    @param varname (string) The key to retrieve from the settings file
+    @param default (string) A default value to return when the setting is not present
+    @returns The configuration value for the key passed. 
+  */
   public function get($varname, $default = null) {
     $section = "config_" . $this->env;
     $value = $default;
@@ -30,6 +54,14 @@ class Config {
     return $value;
   }
   
+  /**
+    Accessor for the settings file to retrieve a section defined
+    outside of the standard config section
+    
+    @param section (string) Name of the section to return
+    @returns Array of key-value pairs in the passed section
+    @throws ConfigException when section has not been found
+  */
   public function getSection($section) {
     if(array_key_exists($section, $this->config)) {
       return $this->config[$section];
