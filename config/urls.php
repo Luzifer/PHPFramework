7a251a4fad76b8102a634276dfd5c46e5ceccb14
Knut Ahlers <knut@ahlers.me>
2011-11-15 13:34:59 +0100

    Converted to better readable comment

--- config/urls.php
+++ config/urls.php
@@ -1,7 +1,9 @@
 <?php
 
-// I suggest to use | as an regex delimiter because in URLs you
-// will use / a lot and have to escape them all otherwise.
+/*
+ * I suggest to use | as an regex delimiter because in URLs you
+ * will use / a lot and have to escape them all otherwise.
+ */
 $urlpattern = array(
   '|^/$|' => 'MainHandler',
   '|^/login/$|' => 'LoginHandler'
