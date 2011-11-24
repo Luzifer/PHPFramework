a5690a07ce1303e70d8eacb28a072d5f40cc51e9
Martin Thielecke <martin@versionzwei.com>
2011-11-24 18:29:38 +0800

    - added add_function method - added env handlers

--- lib/kirby/plugins/twig.php
+++ lib/kirby/plugins/twig.php
@@ -6,12 +6,17 @@ class tpl {
 	
 	static public $vars = array();
   static public $filters = array();
+  static public $functions = array();
 
-  function add_filter($name, $method) {
+  public static function add_function($name, $method) {
+    self::$functions[$name] = $method;
+  }
+
+  public static function add_filter($name, $method) {
     self::$filters[$name] = $method;
   }
 
-	function set($key, $value=false) {
+	public static function set($key, $value=false) {
 		if(is_array($key)) {
 			self::$vars = array_merge(self::$vars, $key);
 		} else {
@@ -19,22 +24,43 @@ class tpl {
 		}
 	}
 
-	function get($key=null, $default=null) {
+	public static function get($key=null, $default=null) {
 		if($key===null) return (array)self::$vars;
 		return a::get(self::$vars, $key, $default);				
 	}
 
-	function load($template='default', $vars=array(), $return=false) {		
-    $file = c::get('twig.root') . '/' . $template . '.html';
+  public static function get_rendered_page($template='default', $vars=array(), $return=false) {
+    $tpl = self::get_template_env($template, $vars, $return);    
+    return $tpl->render(array_merge(self::$vars, $vars));
+  }
+
+  public static function load($template='default', $vars=array(), $return=false) {
+    $tpl = self::get_template_env($template, $vars, $return);    
+    $tpl->display(array_merge(self::$vars, $vars));
+  }
+
+  public static function get_template_env($template='default', $vars=array(), $return=false) {		
+    $file = c::get('twig.root') . c::get('twig.tpldir', '') .'/' . $template . '.html';
     if(!file_exists($file)) return false;
 
     require_once(c::get('root') .'/plugins/Twig/Autoloader.php');
     Twig_Autoloader::register();
-    $loader = new Twig_Loader_Filesystem(c::get('twig.root'));
+
+    $loader = new Twig_Loader_Filesystem(c::get('twig.root') . c::get('twig.tpldir'));
     $twig = new Twig_Environment($loader, array(
       'cache' => c::get('twig.cache', c::get('twig.root') .'/cache'),
       'debug' => c::get('twig.debug', false),
     ));
+
+    if(!empty(self::$functions)) {
+      foreach(self::$functions as $key => $value) {
+        if(is_array($value)) {
+          $twig->addFunction($key, new Twig_Function_Function($value[0] .'::'. $value[1]));
+        } else {
+          $twig->addFunction($key, new Twig_Function_Function($value));
+        }
+      }
+    }
 
     if(!empty(self::$filters)) {
       foreach(self::$filters as $key => $value) {
@@ -46,7 +72,8 @@ class tpl {
       }
     }
     $template = $twig->loadTemplate($template .'.html');
-    $template->display(array_merge(self::$vars, $vars));
+    return $template;
+    
 	}
 
 }
