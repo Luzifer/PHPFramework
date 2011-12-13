5541695080e4ef3d59b890398dd190c135b9e958
Knut Ahlers <knut@ahlers.me>
2011-12-13 12:44:44 +0100

    Converted documentation of HttpHandler to clean PHPDoc

--- classes/HttpHandler.php
+++ classes/HttpHandler.php
@@ -27,18 +27,18 @@
  */
 
 /**
-  Base class for any handler used in urls.php
-  
-  @author Knut Ahlers <knut@ahlers.me>
-*/
+ * Base class for any handler used in urls.php
+ * 
+ * @author Knut Ahlers <knut@ahlers.me>
+ */
 class HttpHandler {
   
   /**
-    Short call to do a redirect to a new URL
-    
-    @param url (string) The full URL used in the redirect
-    @param code (int) May be 301 (permanent redirect) or 302 (temporary redirect)
-  */
+   * Short call to do a redirect to a new URL
+   * 
+   * @param string $url The full URL used in the redirect
+   * @param int $code May be 301 (permanent redirect) or 302 (temporary redirect)
+   */
   protected function redirect($url, $code = 302) {
     if($code == 301) {
       header("HTTP/1.1 301 Moved Permanently");
@@ -48,45 +48,45 @@ class HttpHandler {
   }
   
   /**
-    Wrapper for template loading
-    
-    @param template (string) Name of the template to execute
-  */
+   * Wrapper for template loading
+   *
+   * @param string $template Name of the template to execute
+   */ 
   protected function do_layout($template) {
     tpl::load($template);
   }
   
   /**
-    Handler for GET requests
-    
-    @param params (string[]) Selections from the url defined in urls.php are passed to this
-    @throws HttpResponderException When not implemented in child class
-  */
+   * Handler for GET requests
+   * 
+   * @param array $params Selections from the url defined in urls.php are passed to this
+   * @throws HttpResponderException When not implemented in child class
+   */
   public function get($params) { throw new HttpResponderException('Method GET not implemented for ' . get_class($this)); }
   
   /**
-    Handler for HEAD requests
-
-    @param params (string[]) Selections from the url defined in urls.php are passed to this    
-    @throws HttpResponderException When not implemented in child class
-  */
+   * Handler for HEAD requests
+   *
+   * @param array $params Selections from the url defined in urls.php are passed to this    
+   * @throws HttpResponderException When not implemented in child class
+   */
   public function head($params) { throw new HttpResponderException('Method HEAD not implemented for ' . get_class($this)); }
   
   /**
-    Handler for POST requests
-    
-    @param params (string[]) Selections from the url defined in urls.php are passed to this
-    @throws HttpResponderException When not implemented in child class
-  */
+   * Handler for POST requests
+   * 
+   * @param array $params Selections from the url defined in urls.php are passed to this
+   * @throws HttpResponderException When not implemented in child class
+   */
   public function post($params) { throw new HttpResponderException('Method POST not implemented for ' . get_class($this)); }
 
 
   /**
-    Handler for CLI requests
-    
-    @param params (string[]) Selections from the url defined in urls.php are passed to this
-    @throws HttpResponderException When not implemented in child class
-  */
+   * Handler for CLI requests
+   * 
+   * @param array $params Selections from the url defined in urls.php are passed to this
+   * @throws HttpResponderException When not implemented in child class
+   */
   public function cli($params) { throw new HttpResponderException('Method CLI not implemented for ' . get_class($this)); }
 }
 
