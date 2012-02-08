f9d5a2e3fb08b0c54875fe33eceaac20b5e13d3c
Knut Ahlers <knut@ahlers.me>
2012-02-08 23:44:04 +0100

    Added incomplete test for BaseHttpHandler

---
+++ test/BaseHttpHandlerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+require dirname(__file__) . '/../classes/BaseHttpHandler.php';
+
+class BaseHttpHandlerTest extends PHPUnit_Framework_TestCase {
+  
+  public function testGet() {
+    $this->setExpectedException('MethodNotImplementedException');
+    
+    $handler = new BaseHttpHandler(null, null);
+    $handler->get(null);
+    
+    $this->fail('No exception has been thrown.');
+  }
+  
+  public function testPost() {
+    $this->setExpectedException('MethodNotImplementedException');
+    
+    $handler = new BaseHttpHandler(null, null);
+    $handler->post(null);
+    
+    $this->fail('No exception has been thrown.');
+  }
+  
+  public function testHead() {
+    $this->setExpectedException('MethodNotImplementedException');
+    
+    $handler = new BaseHttpHandler(null, null);
+    $handler->head(null);
+    
+    $this->fail('No exception has been thrown.');
+  }
+  
+  public function testCLI() {
+    $this->setExpectedException('MethodNotImplementedException');
+    
+    $handler = new BaseHttpHandler(null, null);
+    $handler->cli(null);
+    
+    $this->fail('No exception has been thrown.');
+  }
+  
+}
