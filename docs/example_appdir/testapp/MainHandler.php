bb571c65326b999803b39a8ea1ce8dc0e54c310a
Knut Ahlers <knut@ahlers.me>
2012-02-23 18:17:39 +0100

    Added example for template rendering

--- docs/example_appdir/testapp/MainHandler.php
+++ docs/example_appdir/testapp/MainHandler.php
@@ -4,6 +4,9 @@ class TestappMainHandler extends BaseHttpHandler {
 
   public function get($params) {
     // TODO: Write your own code
+
+    $this->response->set('tplvar', 'test');
+    $this->response->display('test_template');
   }
 
 }
