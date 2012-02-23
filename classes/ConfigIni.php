9fd628e87d9b8a9b060556e57b1f4abe1b652699
Knut Ahlers <knut@ahlers.me>
2012-02-23 15:51:43 +0100

    Added option to use a local override config

--- classes/ConfigIni.php
+++ classes/ConfigIni.php
@@ -4,15 +4,20 @@ require_once(dirname(__FILE__) . '/IConfigReader.php');
 
 class ConfigIni implements IConfigReader {
   private $config = null;
+  private $local_config = null;
 
   /**
-   * @param string$config_file_path Path to the INI file to load for config
+   * @param string $config_file_path Path to the INI file to load for config
+   * @param string $local_override_config Path to a local INI file to override the global config
    */
-  public function __construct($config_file_path) {
+  public function __construct($config_file_path, $local_override_config = null) {
     if(!file_exists($config_file_path)) {
       throw new ConfigIniFileNotFoundException('Config file ' . $config_file_path . ' was not found.');
     }
     $this->config = parse_ini_file($config_file_path, true);
+    if($local_override_config !== null) {
+      $this->local_config = parse_ini_file($local_override_config, true);
+    }
   }
 
   /**
@@ -24,6 +29,12 @@ class ConfigIni implements IConfigReader {
    * @throws ConfigIniSectionNotFoundException when section "config" does not exist
    */
   public function get($config_key, $default = null) {
+    if($this->local_config !== null && array_key_exists('config', $this->local_config)) {
+      if(array_key_exists($config_key, $this->local_config['config'])) {
+        return $this->local_config['config'][$config_key];
+      }
+    }
+
     if(!array_key_exists('config', $this->config)) {
       throw new ConfigIniSectionNotFoundException('Default config section "config" was not found.');
     }
@@ -41,6 +52,10 @@ class ConfigIni implements IConfigReader {
    * @throws ConfigIniSectionNotFoundException when section $config_section_name does not exist
    */
   public function getSection($config_section_name) {
+    if(array_key_exists($config_section_name, $this->local_config)) {
+      return $this->local_config[$config_section_name];
+    }
+    
     if(array_key_exists($config_section_name, $this->config)) {
       return $this->config[$config_section_name];
     } else {
