80112e0e85fba1062688f4c41964670e1862430d
Knut Ahlers <knut@ahlers.me>
2012-06-15 18:16:34 +0200

    Fix: Path to dispatcher not longer set by script

--- docs/example_appdir/index.php
+++ docs/example_appdir/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '%%FRAMEWORK%%/dispatcher.php';
+require_once 'lib/framework/dispatcher.php';
 
 // BaseAutoLoader::register_base_lib('mysql');
 // BaseAutoLoader::register_base_lib('couchdb');
