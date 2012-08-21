631646859f2fab45158f68a0d577f0748b1a200c
Knut Ahlers <knut@ahlers.me>
2012-08-21 16:26:18 +0200

    Added first draft of SimpleAuthHandler

---
+++ lib/simpleauth/SimpleAuthHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+class SimpleAuthHandler extends BaseHttpHandler {
+
+  public function __construct($request, $response, $config) {
+    parent::__construct($request, $response, $config);
+
+    $this->check_auth();
+  }
+
+  private function check_auth() {
+    $auth_data = array();
+
+    try {
+      $auth_data = $this->config->getSection('authorization');
+    } catch(Exception $ex) {
+      throw new SimpleAuthConfigurationError('Please provice "authorization" config section.');
+    }
+
+    if(!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
+      $this->send_to_auth();
+    }
+
+    if(!array_key_exists($_SERVER['PHP_AUTH_USER'], $auth_data)) {
+      $this->send_to_auth();
+    }
+
+    $password_hash = $auth_data[$_SERVER['PHP_AUTH_USER']];
+    if(sha1($_SERVER['PHP_AUTH_PW']) !== $password_hash) {
+      $this->send_to_auth();
+    }
+  }
+
+  private function send_to_auth() {
+    $realm = $this->config->get('auth_realm', 'Please login!');
+    header('WWW-Authenticate: Basic realm="' . $realm . '"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo('Login required!');
+    exit;
+  }
+
+}
+
+class SimpleAuthConfigurationError extends Exception {}
