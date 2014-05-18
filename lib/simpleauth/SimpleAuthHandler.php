2d5b1013dddfd5e42bd0158759dc7138dc0ecbc1
Knut Ahlers <knut@ahlers.me>
2014-05-18 23:51:15 +0200

    Replaced SimpleAuthHandler with new version
    This new version has cleaner code and does support the environment
    variable HTTP_AUTHORIZATION for PHP processes running on CGI.

--- lib/simpleauth/SimpleAuthHandler.php
+++ lib/simpleauth/SimpleAuthHandler.php
@@ -1,53 +1,86 @@
 <?php
 
-class SimpleAuthHandler extends BaseHttpHandler {
+/**
+ * Wrapper around the PHP Basic-Authentication functionality including
+ * evaluation of the environment variable 'HTTP_AUTHORIZATION' for GGI
+ * processes.
+ *
+ * @author _Tomalak (https://twitter.com/_Tomalak)
+ */
+class HttpCredential {
+  /**
+   * @var string|null
+   */
+  private $name = null;
+  /**
+   * @var string|null
+   */
+  private $pass = null;
+  /**
+   * @var IConfigReader|null
+   */
+  private $config = null;
 
   /**
-   * Name of the currently authenticated user, null if none
-   *
-   * @var null|string
+   * @param IConfigReader $config
    */
+  public function __construct($config) {
+    $this->config = $config;
+    if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+      $this->user = $_SERVER['PHP_AUTH_USER'];
+      $this->pass = $_SERVER['PHP_AUTH_PW'];
+    } elseif (isset($_ENV['HTTP_AUTHORIZATION'])) {
+      if (preg_match('/^Basic\s+(.+)/i', $_ENV['HTTP_AUTHORIZATION'], $matches)) {
+        $vals = explode(':', base64_decode($matches[1]), 2);
+        $this->name = $vals[0];
+        $this->pass = $vals[1];
+      }
+    }
+  }
+
+  /**
+   * Read the name of the current discovered user. This does not check for
+   * valid credentials.
+   *
+   * @return string|null
+   */
+  public function get_name() {
+    return $this->name;
+  }
+
+  /**
+   * Check whether the passed credentials are available and valid
+   *
+   * @return bool
+   */
+  public function is_authenticated() {
+    $authorization = $this->config->getSection('authorization');
+    return $this->$name !== null
+    && $this->pass !== null
+    && array_key_exists($this->user, $authorization)
+    && sha1($this->pass) !== $authorization[$this->user];
+  }
+}
+
+class SimpleAuthHandler extends BaseHttpHandler {
   protected $authenticated_user = null;
 
   public function __construct($request, $response, $config) {
     parent::__construct($request, $response, $config);
 
-    $this->check_auth();
-  }
-
-  private function check_auth() {
-    $auth_data = array();
-
-    try {
-      $auth_data = $this->config->getSection('authorization');
-    } catch(Exception $ex) {
-      throw new SimpleAuthConfigurationError('Please provice "authorization" config section.');
-    }
-
-    if(!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
+    $current_user = new HttpCredential($this->config);
+    if ($current_user->is_authenticated()) {
+      $this->authenticated_user = $current_user->get_name();
+    } else {
       $this->send_to_auth();
     }
-
-    if(!array_key_exists($_SERVER['PHP_AUTH_USER'], $auth_data)) {
-      $this->send_to_auth();
-    }
-
-    $password_hash = $auth_data[$_SERVER['PHP_AUTH_USER']];
-    if(sha1($_SERVER['PHP_AUTH_PW']) !== $password_hash) {
-      $this->send_to_auth();
-    }
-
-    $this->authenticated_user = $_SERVER['PHP_AUTH_USER'];
   }
 
   private function send_to_auth() {
     $realm = $this->config->get('auth_realm', 'Please login!');
-    header('WWW-Authenticate: Basic realm="' . $realm . '"');
     header('HTTP/1.0 401 Unauthorized');
+    header('WWW-Authenticate: Basic realm="' . $realm . '"');
     echo('Login required!');
     exit;
   }
-
 }
-
-class SimpleAuthConfigurationError extends Exception {}
